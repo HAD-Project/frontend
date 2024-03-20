b81697b415f0b3d97beca8da843c3f69c60d006e
Y0ungSmith <Tarun.Sharma@iiitb.ac.in>
2024-03-20 12:35:16 +0530

    Merged all branches successfully

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import PageRoutes from "./routes";
 import "./assets/styles/styles.css"
-import DoctorList from "./pages/admin/components/DoctorList/DoctorList";
-import Doctor from "./pages/doctor";
-import Patient from "./pages/patient";
-
-import AdminReception from "./pages/adminReceptionists";
-import { Route } from "react-router-dom";
 function App() {
   return (
     <div>
       <PageRoutes/>
-    <Route path="/doctor" element={<Doctor />} />
-        <Route path="/patient" element={<Patient />} />
-        <Route path="/admin" element={<><DoctorList/></>} />
-        <Route path="/adminreceptionists" element={<AdminReception/>}/>
+      
     </div>
   );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,10 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import ReceptionistDashboard from "../pages/RecptionistDashboard";
 import ReceptionistPatients from "../pages/ReceptionistPatients";
+import DoctorList from "../pages/admin/components/DoctorList/DoctorList";
+import Doctor from "../pages/doctor";
+import Patient from "../pages/patient";
+import AdminReception from "../pages/adminReceptionists";
 // import PageNavigation from "../Components/Navigation";
 
 const PageRoutes = () => {
@@ -23,7 +27,10 @@ const PageRoutes = () => {
           path="/receptionist/patients"
           element={<ReceptionistPatients />}
         />
-
+        <Route path="/doctor" element={<div><Doctor /></div>} />      
+            <Route path="/patient" element={<div><Patient /></div>} />
+            <Route path="/admin" element={<div><DoctorList/></div>} />
+            <Route path="/adminreceptionists" element={<div><AdminReception/></div>}/>
         {/* doctor */}
         {/* admin */}
         {/* not match */}
